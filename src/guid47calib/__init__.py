5e2f7abf6d46898d260dd41023a26ed92f4e0ab7
mdaeron <mathieu@daeron.fr>
2025-11-27 11:19:47 +0100

    Add some metadata

--- src/guid47calib/__init__.py
+++ src/guid47calib/__init__.py
@@ -1,2 +1,9 @@
+__author__    = 'Mathieu Daëron'
+__contact__   = 'daeron@lsce.ipsl.fr'
+__copyright__ = 'Copyright (c) 2025 Mathieu Daëron'
+__license__   = 'MIT License - https://opensource.org/licenses/MIT'
+__date__      = '2025-11-27'
+__version__ = '0.1.0'
+
 def main() -> None:
     print("Hello from guiD47calib!")
