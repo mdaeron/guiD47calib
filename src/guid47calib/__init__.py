f50c91f0930df1bb36bb4eca2077cd73b61e5683
mdaeron <mathieu@daeron.fr>
2025-11-27 16:49:06 +0100

    Go lowercase again

--- src/guid47calib/__init__.py
+++ src/guid47calib/__init__.py
@@ -1,9 +1,13 @@
+"""
+An attempt to package a D47calib streamlit app
+"""
+
 __author__    = 'Mathieu Daëron'
 __contact__   = 'daeron@lsce.ipsl.fr'
 __copyright__ = 'Copyright (c) 2025 Mathieu Daëron'
 __license__   = 'MIT License - https://opensource.org/licenses/MIT'
 __date__      = '2025-11-27'
-__version__   = '0.1.2'
+__version__   = '0.1.3'
 
 def main():
 	print('Hello')
