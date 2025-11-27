3184f6005f9f5d1bf47e7bd7013ce561c143012d
mdaeron <mathieu@daeron.fr>
2025-11-27 11:37:28 +0100

    Bump version

--- src/guid47calib/__init__.py
+++ src/guid47calib/__init__.py
@@ -3,7 +3,7 @@ __contact__   = 'daeron@lsce.ipsl.fr'
 __copyright__ = 'Copyright (c) 2025 Mathieu Daëron'
 __license__   = 'MIT License - https://opensource.org/licenses/MIT'
 __date__      = '2025-11-27'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 def main() -> None:
     print("Hello from guiD47calib!")
